ea838bbd065d8728e76893830bf4fabe0b9c6165
Reem Ahmad <reemhawsawi59@gmail.com>
2026-08-05 08:00:29 +0300

    Add post deployment verification

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "v2 - hello from CI"
+
+
+@app.route("/health")
+def health():
+    return "v2 - hello from CI"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
